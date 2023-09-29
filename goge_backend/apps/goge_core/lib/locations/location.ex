f9dd8b3a219340b2a9ad480ee99eea95d55ddf19
Ilya Kuzmin <kuzmin@tochka.com>
2023-09-29 15:35:08 +0400

    prepopulated values

--- goge_backend/apps/goge_core/lib/locations/location.ex
+++ goge_backend/apps/goge_core/lib/locations/location.ex
@@ -7,4 +7,25 @@ defmodule GoGeCore.Location do
   schema "location" do
     field :name, :string
   end
+
+  def values() do
+    [
+      %__MODULE__{
+        id: 1,
+        name: "Tbilisi"
+      },
+      %__MODULE__{
+        id: 2,
+        name: "Batumi"
+      },
+      %__MODULE__{
+        id: 3,
+        name: "Ureqi"
+      },
+      %__MODULE__{
+        id: 4,
+        name: "Qutaisi"
+      }
+    ]
+  end
 end
